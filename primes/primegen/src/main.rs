89049e44241553e8c90ac16f6a8861acf83ba4a9
Sean Timm <seantimm3@aol.com>
2021-01-17 22:47:59 -0700

    print primes below a certain number. inefficient.

--- primes/primegen/src/main.rs
+++ primes/primegen/src/main.rs
@@ -9,13 +9,22 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let max = guess.trim().parse::<i32>().unwrap();
+    let max = guess.trim().parse::<i64>().unwrap();
 
     let vector = 1..max;
 
-    let result = vector.map(|x| x * 2).map(|x| x * 2).collect::<Vec<i32>>();
+    let result = vector.filter(|x| !is_prime(*x)).collect::<Vec<i64>>();
 
-    println!("Mapped: {:?}", result);
+    
+    println!("Primes :  {:?}", result);
 
-    println!("Max Value : {}", max.to_string());
+    println!("Value Printed To : {}.", max.to_string());
+}
+
+fn is_prime(num:i64) -> bool{
+    let vector = 2..(num/2);
+
+    let not_prime = vector.map(|x| num % x == 0).fold(false, |x,y| x || y);
+
+    return not_prime;
 }
